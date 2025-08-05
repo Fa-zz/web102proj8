8239cbcb7446691b6e2bedf245b2d12eb8fa9de1
Fa-zz <fansar8712@gmail.com>
2025-08-05 17:38:56 -0400

    Sorting, like button functionality, comments
    Moved like button stuff out of Card, now it just handled by read

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,32 +1,16 @@
-import { useState, useEffect } from 'react'
 import { Link, useNavigate } from 'react-router-dom'
-import { supabase } from '../client'
 import dayjs from 'dayjs';
 import relativeTime from 'dayjs/plugin/relativeTime';
 import './Card.css'
 
 const Card = (props) =>  {
+    // const [likeCount, setLikeCount] = useState(props.like_count)
+
     dayjs.extend(relativeTime);
 
-    const navigate = useNavigate();
+    // const navigate = useNavigate();
 
     const readable = dayjs(props.created_at).format("MMMM D, YYYY h:mm A");
-
-    useEffect(() => {
-    }, []);
-
-    console.log(props.body);
-    console.log(props.img_url);
-    // const deleteMember = async (event) => {
-    //     event.preventDefault();
-
-    //     await supabase
-    //         .from('party')
-    //         .delete()
-    //         .eq('id', props.id); 
-
-    //     navigate('/');
-    // }
 
     return (
         <div className="tweet-container">
@@ -42,7 +26,9 @@ const Card = (props) =>  {
                     <div className="tweet-footer">
                         <div className="tweet-time">{readable}</div>
                         <div className="card-buttons">
-                            <button className="like-btn">❤️ {props.like_count}</button>
+                            <button onClick={() => props.onClickLike(props.id, props.like_count)} className="like-btn">
+                            ❤️ {props.like_count}
+                            </button>
                             <button className="retweet-btn">🔁</button>
                         </div>
                     </div>
--- src/pages/Read.jsx
+++ src/pages/Read.jsx
@@ -1,51 +1,140 @@
 import { useState, useEffect } from 'react'
 import Card from '../components/Card'
 import { supabase } from '../client'
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
+import dayjs from 'dayjs';
 import './Read.css'
 
-const Read = () => {
-    const [posts, setPosts] = useState([])
+// Ascending, descending date and like count sort
+const descDateSort = (a, b) => dayjs(b.created_at).valueOf() - dayjs(a.created_at).valueOf();
+const ascDateSort = (a, b) =>  dayjs(a.created_at).valueOf() - dayjs(b.created_at).valueOf();
+const descLikeSort = (a, b) => b.like_count - a.like_count;
+const ascLikeSort = (a, b) =>  a.like_count - b.like_count;
 
-    useEffect(() => {
-        // READ all post from table
-        const fetchPosts = async () => {
-            try {
-                const {data} = await supabase
-                    .from('posts')
-                    .select();
-                // set state of posts
+const Read = () => {
+    const navigate = useNavigate();
+    const [posts, setPosts] = useState([]) // acquired from querying db after initial render, and when like button is clicked (see Card component)
+    const [sortDateScheme, setDateSortScheme] = useState('a'); // the default scheme to sort date. 'd' for desc, 'a' for asc
+    const [sortLikeScheme, setLikeSortScheme] = useState('a'); // the default scheme to sort likes
+    const [sortingWhat, setSortingWhat] = useState('d'); // posts can only be sorted by one value at a time. This controls what is being sorted. 'd' for date, 'l' for likes
+
+    // Returns the correct sorting function given the sorting scheme
+    const sortDateOrder = (sortingScheme) => {
+        if (sortingScheme === "a") {
+            return ascDateSort;
+        } else if (sortingScheme == "d") {
+            return descDateSort;
+        }
+    }
+
+    // Returns the correct sorting function given the sorting scheme
+    const sortLikeOrder = (sortingScheme) => {
+        if (sortingScheme === "a") {
+            return ascLikeSort;
+        } else if (sortingScheme == "d") {
+            return descLikeSort;
+        }
+    }
+
+    // When "date" sort button is clicked, this function sets sortingWhat to date, and flips the dateSortScheme
+    const onClickSortDate = (event) => {
+        event.preventDefault();
+        setSortingWhat('d');
+        sortDateScheme === 'a' ? setDateSortScheme('d') : setDateSortScheme('a')
+        navigate('/');
+    }
+
+    // When "like" sort button is clicked, this function sets sortingWhat to like, and flips the likeSortScheme
+    const onClickSortLike = (event) => {
+        event.preventDefault();
+        setSortingWhat('l');
+        sortLikeScheme === 'a' ? setLikeSortScheme('d') : setLikeSortScheme('a')
+        navigate('/');
+    }
+
+    // Gets all posts
+    const fetchPosts = async () => {
+        try {
+            const {data} = await supabase
+                .from('posts')
+                .select();
+            // set state of posts
                 setPosts(data)
             } catch (error) {
                 console.log("Error reading posts ", error)
             }
-        }  
+    }  
+
+    // When page initially loads, get all posts
+    useEffect(() => {
+        // READ all post from table
         fetchPosts();
     }, [])
-    
+
+    // Gets a specific post by id (currently unused)
+    const selectDataById = async (targetId) => {
+        const { data, error } = await supabase
+            .from('posts')
+            .select('*') // Selects all columns. You can specify specific columns like 'name, email'
+            .eq('id', targetId); // Filters where the 'idColumnName' matches 'targetId'
+
+        if (error) {
+            console.error('Error fetching data:', error.message);
+            return null;
+        }
+        return data;
+    }
+
+    // Called when like button is clicked in Card.jsx. Increments the like count of a post, then re-fetches all posts
+    const updateLikeCount = async (id, oldLikeCount) => {
+        const newLikeCount = oldLikeCount + 1;
+
+        const { error } = await supabase
+            .from('posts')
+            .update({ like_count: newLikeCount })
+            .eq('id', id);
+
+        if (error) {
+            console.error("Error updating like count:", error.message);
+        }
+        fetchPosts();
+        navigate('/');
+    }
+
     return (
         <div>
             <h3 style={{ textAlign: 'center' }}>All posts</h3>
             <div className="filters">
                 <p>Sort:</p>
-                <button>Date</button>
-                <button>Likes</button>
+                <button onClick={onClickSortDate}>Date</button>
+                <button onClick={onClickSortLike}>Likes</button>
                 <button>Retweets</button>
             </div>
             {
                 posts && posts.length > 0 ?
                 [...posts]
-                .sort((a, b) => a.id - b.id)
-                .map((member,index) => 
+                .sort((a, b) => {
+                    if (sortingWhat === 'd') {
+                        const dateSortFn = sortDateOrder(sortDateScheme);
+                        return dateSortFn(a, b);
+                    } else if (sortingWhat == 'l') {
+                        const likeSortFn = sortLikeOrder(sortLikeScheme);
+                        return likeSortFn(a, b);
+                    }
+                })
+                .map((member, index) => (
                     <Card
-                        created_at={member.created_at} 
-                        body={member.body}                  
-                        like_count={member.like_count}
-                        author={member.author}
-                        img_url={member.img_url}
-                        title={member.title}
+                    id={member.id}
+                    created_at={member.created_at}
+                    body={member.body}
+                    like_count={member.like_count}
+                    author={member.author}
+                    img_url={member.img_url}
+                    title={member.title}
+                    onClickLike={updateLikeCount}
+                    key={member.id}
                     />
-                ) : <h2>{'No members in posts 😞'}</h2>
+                )) : <h2>{'No members in posts 😞'}</h2>
             }
         </div>  
     )
